40a7243db813486bfadbe56a62efd90b9ddb183f
Kevin K <ktkinsey37@gmail.com>
2021-04-27 18:49:59 -0600

    final commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import random, requests
 
 app = Flask(__name__)
+
+base_url = "http://numbersapi.com"
 
 
 @app.route("/")
@@ -11,19 +14,38 @@ def homepage():
 
 @app.route("/api/get-lucky-num", methods=["POST"])
 def json_lucky_num():
-    name = request.json["name"]
-    email = request.json["email"]
-    year = request.json["year"]
-    color = request.json["color"]
+
+    try:
+        name = request.json["name"]
+    except:
+        name = ''
+    try:
+        email = request.json["email"]
+    except:
+        email = ''
+    try:
+        year = request.json["year"] 
+    except:    
+        year = "0000"
+    try:
+        color = request.json["color"]
+    except:
+        color = ''
 
     # Validates incoming information by building a failure object
     failed_lucky_num = validate_lucky_num_input(name, email, year, color)
 
     if failed_lucky_num:
-        return (jsonify(failed_lucky_num), 201)
+        return (jsonify(failed_lucky_num))
 
-    lucky_num = {"num": {}, "year": {} }
+    random_number = random.randint(1, 100)
+    headers = {'content-type': 'application/json'}
 
+    num_response = requests.get(f'{base_url}/{random_number}', headers=headers).json()
+    year_response = requests.get(f'{base_url}/{year}/year', headers=headers).json()
+
+    lucky_num_output = {"num": {"fact": num_response["text"], "num" : random_number}, "year": {"fact": year_response["text"], "year": year} }
+    return jsonify(lucky_num_output)
     
 
 
@@ -38,7 +60,11 @@ def validate_lucky_num_input(name, email, year, color):
     if not email:
         error_message["errors"]["email"] = ["Email is required"]
         errors = True
-    if int(year) > 2000 or int(year) < 1900:
+    try:
+        if int(year) > 2000 or int(year) < 1900:
+            error_message["errors"]["year"] = ["Year is required and must be between 1900 and 2000"]
+            errors = True
+    except:
         error_message["errors"]["year"] = ["Year is required and must be between 1900 and 2000"]
         errors = True
     if color not in valid_colors:
--- static/lucky.js
+++ static/lucky.js
@@ -1,12 +1,29 @@
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
+async function processForm(evt) {
+    evt.preventDefault();
+    const name = $("#name").val();
+    const year = $("#year").val();
+    const email = $("#email").val();
+    const color = $("#color").val();
+    const results = await axios.post("/api/get-lucky-num", { "name": name, "year": year, "email": email, "color": color});
+    handleResponse(results)
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
-function handleResponse(resp) {
-}
+async function handleResponse(resp) {
+    if (resp.data.errors){
+        for (const error in resp.data.errors) {
+            $(`#${error}-err`).text(resp.data.errors[error])
+          }
+        }
+    else {
+        console.log(resp.data.num.num)
+        $('#lucky-results').text(`Your lucky number is ${resp.data.num.num} (${resp.data.num.fact}). Your birth year (${resp.data.year.year}) fact is ${resp.data.year.fact}.`)
+    }
+    }
+
 
 
 $("#lucky-form").on("submit", processForm);
